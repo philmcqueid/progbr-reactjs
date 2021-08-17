e688f7fe811f060c82ce76f941ebde387a3ca2ae
philmcqueid <philmcqueid@gmail.com>
2021-08-16 22:59:40 -0300

    Iniciando Hooks

--- meu-app/meu-app/src/App.js
+++ meu-app/meu-app/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Header";
+import Counter from "./Counter";
 import "./App.css";
 
 function App() {
     return (
         <div>
             <Header name="Phil" links={["Inicial", "Galeria", "Imagens", "Telefones", "Geladeira"]}></Header>
+            <Counter count={3}></Counter>
         </div>
     );
 }
